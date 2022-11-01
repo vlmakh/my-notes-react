9916d4dbb89eee22a0a903c1255b60d507092944
vlmakh <vlmakh@gmail.com>
2022-11-01 08:00:29 +0200

    add work files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# goit-react-hw-02-phonebook
+# My Todo List
+
+Application is written on React using hooks.
+
+Attached libraries:
+
+- emotions;
+- styled-system;
+- react-icons;
+- nanoid.
+
+Added the ability to add new, mark ready, delete, edit notes.
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
+import { Box, BoxMain } from 'components/Box/Box';
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { TodoList } from './TodoList/TodoList';
+import { AddNew } from './AddNew/AddNew';
+
+const startTodos = [
+  { id: nanoid(4), text: '6:00 Подъем', completed: false },
+  { id: nanoid(4), text: '7:00 Разгон облаков', completed: false },
+  { id: nanoid(4), text: '10:00-13:00 Подвиг', completed: false },
+];
+
+function App() {
+  const savedData = JSON.parse(localStorage.getItem('todos'));
+  const [todos, setTodos] = useState(savedData ? savedData : startTodos);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = data => {
+    const newTodo = {
+      id: nanoid(4),
+      text: data,
+      completed: false,
+    };
+
+    if (data.trim() !== '') {
+      setTodos([newTodo, ...todos]);
+    }
+  };
+
+  const editTodo = (todoId, newText) => {
+    setTodos(todos =>
+      todos.map(todo =>
+        todo.id === todoId ? { ...todo, text: newText } : todo
+      )
+    );
+  };
+
+  const completeTodo = todoId => {
+    setTodos(todos =>
+      todos.map(todo =>
+        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = todoId => {
+    if (global.confirm('Вы действительно хотите удалить?')) {
+      setTodos(todos.filter(todo => todo.id !== todoId));
+    }
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <BoxMain>
+      <Box bg="tomato" p={2} textAlign="center" color="white">
+        <h4>My To-do List</h4>
+      </Box>
+
+      <AddNew onSubmit={addTodo} />
+
+      <TodoList
+        todos={todos}
+        deleteTodo={deleteTodo}
+        editTodo={editTodo}
+        completeTodo={completeTodo}
+      ></TodoList>
+    </BoxMain>
   );
-};
+}
+
+export { App };
