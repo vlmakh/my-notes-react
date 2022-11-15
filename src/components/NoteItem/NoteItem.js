5f3d6fbc80d37e448c79c9fadeb3dd87ed2f1163
vlmakh <vlmakh@gmail.com>
2022-11-15 18:16:26 +0200

    del class-code

--- src/components/NoteItem/NoteItem.js
+++ src/components/NoteItem/NoteItem.js
@@ -7,7 +7,6 @@ import { MdDeleteForever } from 'react-icons/md';
 import { FaSave, FaMarker } from 'react-icons/fa';
 import { SaveBtn, EditBtn, DeleteBtn } from './NoteItem.styled';
 import { NoteEditModal } from 'components/NoteEditModal/NoteEditModal';
-// import { Component } from 'react';
 
 function NoteItem({ note, editNote, deleteNote, editNoteName }) {
   const [todos, setTodos] = useState(note.todos);
@@ -29,7 +28,6 @@ function NoteItem({ note, editNote, deleteNote, editNoteName }) {
         completed: false,
       };
       setTodos([newTodo, ...todos]);
-      // todos = [newTodo, ...todos];
       handleSave();
     }
   };
@@ -120,104 +118,5 @@ function NoteItem({ note, editNote, deleteNote, editNoteName }) {
     </Box>
   );
 }
-
-// class NoteItem extends Component {
-//   state = {
-//     todos: [],
-//   };
-
-//   componentDidMount() {
-//     this.setState({ todos: [...this.props.note.note] });
-//   }
-
-//   componentDidUpdate(_, prevState) {
-//     if (this.state.todos !== prevState.todos) {
-//       this.props.editNote(this.props.note.noteid, this.state.todos);
-//     }
-//   }
-
-//   addTodo = data => {
-//     if (data.trim() !== '') {
-//       const newTodo = {
-//         id: nanoid(4),
-//         text: data,
-//         completed: false,
-//       };
-//       this.setState(prevState => ({ todos: [newTodo, ...prevState.todos] }));
-//     }
-//   };
-
-//   editTodo = (todoId, newText) => {
-//     this.setState(prevState => ({
-//       todos: prevState.todos.map(todo =>
-//         todo.id === todoId ? { ...todo, text: newText } : todo
-//       ),
-//     }));
-//   };
-
-//   completeTodo = todoId => {
-//     this.setState(prevState => ({
-//       todos: prevState.todos.map(todo =>
-//         todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
-//       ),
-//     }));
-//   };
-
-//   deleteTodo = todoId => {
-//     if (global.confirm('Удалить задачу?')) {
-//       this.setState(prevState => ({
-//         todos: prevState.todos.filter(todo => todo.id !== todoId),
-//       }));
-//     }
-//   };
-
-//   render() {
-//     const todos = this.state.todos;
-
-//     return (
-//       <Box>
-//         <Box
-//           width="340px"
-//           mr={3}
-//           mb={3}
-//           border="1px solid grey"
-//           borderRadius="8px"
-//           overflow="hidden"
-//           boxShadow="0px 8px 16px rgba(0, 0, 0, 0.2)"
-//         >
-//           <Box
-//             bg="tomato"
-//             py={2}
-//             px={5}
-//             textAlign="center"
-//             color="white"
-//             display="flex"
-//             justifyContent="space-between"
-//           >
-//             <h4>Note {this.props.note.noteid}</h4>
-//             <EditBtn type="button">
-//               <FaMarker />
-//             </EditBtn>
-//             <DeleteBtn
-//               type="button"
-//               onClick={() => this.props.deleteNote(this.props.note.noteid)}
-//             >
-//               <MdDeleteForever size="20" />
-//             </DeleteBtn>
-//           </Box>
-
-//           <TodoAddNew onSubmit={this.addTodo} />
-
-//           <TodoList
-//             todos={todos}
-//             deleteTodo={this.deleteTodo}
-//             editTodo={this.editTodo}
-//             completeTodo={this.completeTodo}
-//           ></TodoList>
-//         </Box>
-//       </Box>
-//     );
-//   }
-// }
 
 export { NoteItem };
