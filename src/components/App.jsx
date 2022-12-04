dd211baa187ae14dac7a7fef0230af9540255970
vlmakh <vlmakh@gmail.com>
2022-12-04 20:08:24 +0200

    del comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,8 +42,8 @@ const reducer = (mynotes, action) => {
     case 'deleteNote':
       if (global.confirm(`Delete note: ${action.name}?`)) {
         return mynotes.filter(note => note.noteid !== action.noteId);
-      };
-    // eslint-disable-next-line no-fallthrough
+      }
+      break;
     case 'editNoteName':
       return mynotes.map(noteItem => {
         return noteItem.noteid === action.noteId
@@ -57,69 +57,42 @@ const reducer = (mynotes, action) => {
 
 function App() {
   const savedData = JSON.parse(localStorage.getItem('mynotes'));
-  // const [mynotes, setMynotes] = useState(savedData ? savedData : startNotes);
-  const [mynotes, dispatch] = useReducer(reducer, savedData ? savedData : startNotes);
+  const [mynotes, dispatch] = useReducer(
+    reducer,
+    savedData ? savedData : startNotes
+  );
 
   useEffect(() => {
     localStorage.setItem('mynotes', JSON.stringify(mynotes));
   }, [mynotes]);
 
-  // const addNote = () => {
-  //   const newNote = {
-  //     noteid: nanoid(4),
-  //     name: 'New note',
-  //     todos: [],
-  //     color: getRandomHexColor(),
-  //   };
-  //   setMynotes([...mynotes, newNote]);
-  // };
-
-  // const editNote = useCallback(
-  //   (noteId, newTodos) => {
-  //     setMynotes(
-  //       mynotes.map(noteItem => {
-  //         return noteItem.noteid === noteId
-  //           ? { ...noteItem, todos: newTodos }
-  //           : noteItem;
-  //       })
-  //     );
-  //   },
-  //   [mynotes]
-  // );
-
-  // const deleteNote = (noteId, name) => {
-  //   if (global.confirm(`Delete note: ${name}?`)) {
-  //     setMynotes(mynotes.filter(note => note.noteid !== noteId));
-  //   }
-  // };
-
-  // const editNoteName = (noteId, newName) => {
-  //   setMynotes(
-  //     mynotes.map(noteItem => {
-  //       return noteItem.noteid === noteId
-  //         ? { ...noteItem, name: newName }
-  //         : noteItem;
-  //     })
-  //   );
-  // };
-
   return (
-    <MyContext.Provider value={{dispatch}}>
-    <Box p={3} display="flex" flexWrap="wrap">
-      {mynotes.map(noteItem => {
-        return (
-          <NoteItem
-            key={noteItem.noteid}
-            note={noteItem}
-            editNote={(noteId, newTodos) => {dispatch({type: 'editNote',noteId, newTodos })}}
-            deleteNote={(noteId, name) => {dispatch({type: 'deleteNote',noteId, name })}}
-            editNoteName={(noteId, newName) => {dispatch({type: 'editNoteName',noteId, newName })}}
-          />
-        );
-      })}
-      <NoteAddBtn addNote={() => {dispatch({type: 'addNote' })}} />
+    <MyContext.Provider value={{ dispatch }}>
+      <Box p={3} display="flex" flexWrap="wrap">
+        {mynotes.map(noteItem => {
+          return (
+            <NoteItem
+              key={noteItem.noteid}
+              note={noteItem}
+              editNote={(noteId, newTodos) => {
+                dispatch({ type: 'editNote', noteId, newTodos });
+              }}
+              deleteNote={(noteId, name) => {
+                dispatch({ type: 'deleteNote', noteId, name });
+              }}
+              editNoteName={(noteId, newName) => {
+                dispatch({ type: 'editNoteName', noteId, newName });
+              }}
+            />
+          );
+        })}
+        <NoteAddBtn
+          addNote={() => {
+            dispatch({ type: 'addNote' });
+          }}
+        />
       </Box>
-      </MyContext.Provider>
+    </MyContext.Provider>
   );
 }
 
