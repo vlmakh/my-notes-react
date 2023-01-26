13c0d4936dcdbeb377374597717a3fa4d664bbb2
Vlad Makhnenko <vlmakh@gmail.com>
2023-01-26 08:41:06 +0200

    update getRandomColor

--- src/utils/getRandomHexColor.jsx
+++ src/utils/getRandomHexColor.jsx
@@ -1,3 +1,7 @@
 export function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const random = Math.floor(Math.random() * 16777215).toString(16);
+
+  const result = random.length === 5 ? random + '0' : random;
+
+  return `#${result}`;
 }
