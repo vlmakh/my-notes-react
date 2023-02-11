d158d66218e76644cc256a68c175b0a87ab25edd
Vlad Makhnenko <vlmakh@gmail.com>
2023-02-11 07:39:56 +0200

    update operations

--- src/utils/operations.js
+++ src/utils/operations.js
@@ -12,7 +12,7 @@ const token = {
   },
 };
 
-const errorMsg = "Something's wrong. Please update page and try again";
+const errorMsg = "Something's wrong. Please refresh page and try again";
 
 export const signup = async credentials => {
   try {
@@ -29,8 +29,9 @@ export const signup = async credentials => {
 export const login = async credentials => {
   try {
     const response = await axios.post(`/users/login`, credentials);
+
     token.set(response.data.token);
-    // console.log(response.data);
+
     return response.data;
   } catch (error) {
     toast.error('There is mistake in login or password, please try again');
@@ -47,23 +48,23 @@ export const checkCurrentUser = async savedToken => {
   try {
     const response = await axios.get(`/users/current`);
     return response.data;
-  } catch (error) {
-    // toast.error('Please try to login again');
-  }
+  } catch (error) {}
 };
 
 export const logout = async () => {
   try {
     await axios.get(`/users/logout`);
+
     token.unset();
   } catch (error) {
-    toast.error(error);
+    toast.error(error.message);
   }
 };
 
 export const getNotes = async () => {
   try {
     const response = await axios.get(`/notes`);
+
     return response.data;
   } catch (error) {
     toast.error("Something's wrong. Please login again");
@@ -73,10 +74,12 @@ export const getNotes = async () => {
 export const addNote = async newNote => {
   try {
     const response = await axios.post(`/notes`, newNote);
+
     toast.success(`${newNote.name} was added`);
+
     return response.data;
   } catch (error) {
-    toast.error(error);
+    toast.error(errorMsg);
   }
 };
 
@@ -84,7 +87,9 @@ export const deleteNote = async note => {
   const { _id, name } = note;
   try {
     const response = await axios.delete(`/notes/${_id}`);
+
     toast.success(`${name} was deleted`);
+
     return response.data;
   } catch (error) {
     toast.error(errorMsg);
@@ -96,10 +101,12 @@ export const updateNoteName = async (noteId, newName) => {
     const response = await axios.put(`/notes/${noteId}/name`, {
       name: newName,
     });
+
     toast.success(`'${newName}' was updated`);
+
     return response.data;
   } catch (error) {
-    toast.error(error);
+    toast.error(errorMsg);
   }
 };
 
@@ -108,10 +115,12 @@ export const updateNoteColor = async (noteId, newColor) => {
     const response = await axios.put(`/notes/${noteId}/color`, {
       color: newColor,
     });
+
     toast.success(`Note color was updated`);
+
     return response.data;
   } catch (error) {
-    toast.error(error);
+    toast.error(errorMsg);
   }
 };
 
@@ -120,9 +129,9 @@ export const updateNoteTodos = async (noteId, newTodos) => {
     const response = await axios.put(`/notes/${noteId}/todos`, {
       todos: newTodos,
     });
-    // toast.success(`${noteId} todos were updated`);
+
     return response.data;
   } catch (error) {
-    toast.error(error);
+    toast.error(errorMsg);
   }
 };
